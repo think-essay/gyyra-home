999eb645dbaeddc31f840f2254f385da6039297d
think-essay <think-essay@users.noreply.github.com>
2026-08-04 03:31:38 +0000

    chore: update WakaTime theme rest

--- assets/json/config.js
+++ assets/json/config.js
@@ -1,7 +1,7 @@
 window.WAKATIME_CONFIG = {
-  "date": "2026-08-02",
+  "date": "2026-08-03",
   "hours": 0,
   "theme_name": "rest",
   "theme_display": "休息日",
-  "updated_at": "2026-08-03T03:48:22.379Z"
+  "updated_at": "2026-08-04T03:31:37.271Z"
 };
--- assets/json/weekly.js
+++ assets/json/weekly.js
@@ -1,5 +1,5 @@
 window.WAKATIME_WEEKLY = {
-  "updated_at": "2026-08-03T03:48:22.379Z",
+  "updated_at": "2026-08-04T03:31:37.272Z",
   "stats": {
     "total_hours": 0.22,
     "daily_avg": 0.03,
@@ -11,11 +11,6 @@ window.WAKATIME_WEEKLY = {
     }
   },
   "days": [
-    {
-      "date": "2026-07-28",
-      "hours": 0,
-      "text": "0 secs"
-    },
     {
       "date": "2026-07-29",
       "hours": 0,
@@ -43,6 +38,11 @@ window.WAKATIME_WEEKLY = {
     },
     {
       "date": "2026-08-03",
+      "hours": 0,
+      "text": "0 secs"
+    },
+    {
+      "date": "2026-08-04",
       "hours": 0,
       "text": "0 secs"
     }
